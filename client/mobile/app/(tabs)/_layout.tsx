b94bdfb1bc35a78536032d0781357f0739b7c4a9
Anshalll <anshalkumar900@gmail.com>
2025-05-23 22:58:49 +0530

    Create tab layout : mobile

---
+++ client/mobile/app/(tabs)/_layout.tsx
@@ -0,0 +1,59 @@
+import { Tabs } from "expo-router"
+import { Theme } from '../../Theme/ThemeContext'
+import { House, SquarePlus, User, MessageCircle, Menu } from 'lucide-react-native';
+
+const TabLayout = () => {
+    return (
+        <Tabs screenOptions={{
+
+            headerShown: false,
+            tabBarStyle: {
+                backgroundColor: Theme.darkcomp,
+                height: 70,
+               
+            }
+            ,
+            tabBarActiveTintColor: Theme.Secondarycolor,
+
+
+        }}>
+            <Tabs.Screen name="(home)/index" options={{
+                tabBarShowLabel: false,
+                title: "Home", tabBarIcon: ({ color }) => (
+                    <House size={24} color={color} />
+                )
+            }} />
+
+            <Tabs.Screen name="(chat)/index" options={{
+                tabBarShowLabel: false,
+                tabBarIcon: ({ color }) => (
+                    <MessageCircle size={24} color={color} />
+                )
+            }} />
+
+            <Tabs.Screen name="(create)/index" options={{
+                tabBarShowLabel: false,
+                title: "Home", tabBarIcon: ({ color }) => (
+                    <SquarePlus size={24} color={color} />
+                )
+            }} />
+
+            <Tabs.Screen name="(profile)/index" options={{
+                tabBarShowLabel: false,
+                title: "Home", tabBarIcon: ({ color }) => (
+                    <User size={24} color={color} />
+                )
+            }} />
+
+            <Tabs.Screen name="(more)/index" options={{
+                tabBarShowLabel: false,
+                title: "Home", tabBarIcon: ({ color }) => (
+                    <Menu size={24} color={color} />
+                )
+            }} />
+
+
+        </Tabs>
+    )
+}
+export default TabLayout
