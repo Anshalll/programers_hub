e17bbd640fdee17e62fd19e929112fb61a1eaf0c
Anshalll <anshalkumar900@gmail.com>
2025-06-14 12:29:21 +0530

    Mark only last sender message as seen

--- client/web/src/components/chat/ChatMessages.jsx
+++ client/web/src/components/chat/ChatMessages.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Input from '../Inputcomment.jsx'
 import AttachFileIcon from '@mui/icons-material/AttachFile';
 import InsertPhotoOutlinedIcon from '@mui/icons-material/InsertPhotoOutlined';
@@ -15,6 +15,7 @@ export default function ChatMessages({ selectedChat }) {
     const [Users, setUsers] = useState({})
     const socket = useSocket()
     const [Seenlast, setSeenlast] = useState(false)
+    const scrollview = useRef(null)
 
     const [NewMessage, setNewMessage] = useState("")
     useEffect(() => {
@@ -23,40 +24,44 @@ export default function ChatMessages({ selectedChat }) {
         }
     }, [selectedChat.data, data.username])
 
-useEffect(() => {
-    if (Object.keys(Users).length > 0 && socket.connected) {
+    useEffect(() => {
+        if (Object.keys(Users).length > 0 && socket.connected) {
 
-        socket.emit("getchatdata", {
-            "usera": { "name": Users.usera },
-            "userb": { "name": Users.userb }
-        }, (e) => {
-            console.log("Chat data response:", e);
-        });
+            socket.emit("getchatdata", {
+                "usera": { "name": Users.usera },
+                "userb": { "name": Users.userb }
+            }, (e) => {
+                console.log("Chat data response:", e);
+            });
 
-    }
-}, [Users, socket]);
+        }
+    }, [Users, socket]);
 
-useEffect(() => {
-  const handler = (msg) => {
-    
-    setMessages((e) => [...e, msg])
-    socket.emit("markseen" , {
-        "username" : selectedChat.data.username
-    })
-  };
+    useEffect(() => {
+        const handler = (msg) => {
 
-  const markseen = (e) => {
-    console.log(e)
-    setSeenlast(true)
-  }
+            setMessages((e) => [...e, msg])
+            socket.emit("markseen", {
+                "username": selectedChat.data.username
+            })
+        };
 
-  socket.on("receive_message", handler);
-  socket.on("seen_status" , markseen)
+        const markseen = (e) => {
+            console.log(e)
+            setSeenlast(true)
+        }
 
-  return () => {
-    socket.off("receive_message", handler);
-  };
-}, [socket, data.username , Messages , selectedChat]);
+        socket.on("receive_message", handler);
+        socket.on("seen_status", markseen)
+
+        return () => {
+            socket.off("receive_message", handler);
+        };
+    }, [socket, data.username, Messages, selectedChat]);
+    useEffect(() => {
+      
+        scrollview.current?.scrollIntoView({ behavior: "smooth" });
+    }, [Messages]);
 
 
     const SendMessage = () => {
@@ -67,9 +72,10 @@ useEffect(() => {
 
             })
             setSeenlast(false)
-            setMessages((e) => [...e , {"from" : data.username, "message": NewMessage, "type": "sender"}])
+            setNewMessage("")
+            setMessages((e) => [...e, { "from": data.username, "message": NewMessage, "type": "sender" }])
         }
-    }   
+    }
 
     return (
         <div className='w-full h-full flex-col  '>
@@ -81,26 +87,28 @@ useEffect(() => {
                         <p className='text-lg text-white font-bold'>Start a conversation</p>
                     </div> : <></>}
 
-                    
-                    
+
+
                     {Messages.map((item, key) => (
                         <div key={key}>
 
                             <div className={`flex ${item.type === "sender" ? "justify-end" : "justify-start"} w-full`} key={key}>
-                                
-
-                                    <div className='flex flex-col max-w-[70%] h-max items-end gap-[10px] '>
 
 
-                                        <div className='w-full  darkcomp shadow-lg rounded-tl-2xl rounded-tr-md rounded-bl-2xl  shadow-cyan-900/30 p-4'>
-                                            <p className='leading-relaxed text-white'>{item.message}</p>
-                                            { Seenlast &&  <p className='text-white'>Seen</p> }
-                                        </div>
+                                <div className='flex flex-col max-w-[70%] h-max items-end gap-[10px] '>
 
+
+                                    <div className='w-full  darkcomp shadow-lg rounded-tl-2xl rounded-tr-md rounded-bl-2xl  shadow-cyan-900/30 p-4'>
+                                        <p className='leading-relaxed text-white'>{item.message}</p>
+                                        {Seenlast && item.type === "sender" && key === Messages.length - 1 && <p className='text-white'>Seen</p>}
                                     </div>
+
+                                </div>
                             </div>
                         </div>
-                    ))} 
+                    ))}
+
+                    <div ref={scrollview} />
                 </div>
 
                 <div className='flex text-white justify-center items-center w-full h-[80px] darkcomp'>
